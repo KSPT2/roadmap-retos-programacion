78035e6322c64a33effa013a9541d959e77e1e7c
KSPT2 <129570201+KSPT2@users.noreply.github.com>
2024-10-12 13:47:27 -0500

    Create KSPT.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/KSPT.py
@@ -0,0 +1,29 @@
+# https://www.python.org/
+
+# Comentario en una línea
+
+""""
+Comentarios en
+varías líneas 
+""""
+
+'''
+Comentarios en
+varías líneas 
+'''
+
+# Variable
+nombre = "KSPT"
+
+# Pyton no soporta variables constantes
+
+string: str = "Python" # Cadena de carácteres
+numero_entero: int = 20 # Numeros enteros
+numero_real: float = 20.00 # Numeros reales
+booleanos: bool = True # Verdadero o Falso
+
+# Nota: En python el tipado de las variables es dinamico
+
+
+# Imprimiendo en consola
+print("¡Hola Python!")
